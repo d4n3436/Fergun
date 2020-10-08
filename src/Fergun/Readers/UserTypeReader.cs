d2d2d9c92a23203f343d7955e31650e0bed59551
d4n <dan3436@hotmail.com>
2020-10-08 17:57:30 -0500

    SearchUsersAsync() now requires server members intent 🤔

--- src/Fergun/Readers/UserTypeReader.cs
+++ src/Fergun/Readers/UserTypeReader.cs
@@ -34,27 +34,33 @@ namespace Fergun.Readers
                 }
             }
 
-            //IAsyncEnumerable<IUser> channelUsers = context.Channel.GetUsersAsync(CacheMode.CacheOnly).Flatten(); // it's better
+            IAsyncEnumerable<IUser> channelUsers = context.Channel.GetUsersAsync(CacheMode.CacheOnly).Flatten(); // it's better
 
             IReadOnlyCollection<IGuildUser> guildUsers = ImmutableArray.Create<IGuildUser>();
 
-            //if (context.Guild != null)
-            //    guildUsers = await context.Guild.GetUsersAsync(CacheMode.CacheOnly).ConfigureAwait(false);
+            if (context.Guild != null)
+            {
+                // TODO: A way to know if the current user has server members intent and use the method below
+                /*
+                guildUsers = await context.Guild.SearchUsersAsync(input, 10).ConfigureAwait(false);
+                if (guildUsers.Count == 1)
+                {
+                    return TypeReaderResult.FromSuccess(guildUsers.First() as T);
+                }
+                */
+                guildUsers = await context.Guild.GetUsersAsync(CacheMode.CacheOnly).ConfigureAwait(false);
+            }
 
-            guildUsers = await context.Guild.SearchUsersAsync(input, 10).ConfigureAwait(false);
-            if (guildUsers.Count == 1)
-                return TypeReaderResult.FromSuccess(guildUsers.First() as T);
-
-            //By Username + Discriminator (0.7-0.85)
+            //By Username + Discriminator (0.7-0.8)
             int index = input.LastIndexOf('#');
             if (index >= 0)
             {
                 string username = input.Substring(0, index);
                 if (ushort.TryParse(input.Substring(index + 1), out ushort discriminator))
                 {
-                    //var channelUser = await channelUsers.FirstOrDefaultAsync(x => x.DiscriminatorValue == discriminator &&
-                    //    string.Equals(username, x.Username, StringComparison.OrdinalIgnoreCase)).ConfigureAwait(false);
-                    //AddResult(results, channelUser as T, channelUser?.Username == username ? 0.85f : 0.75f);
+                    var channelUser = await channelUsers.FirstOrDefaultAsync(x => x.DiscriminatorValue == discriminator &&
+                        string.Equals(username, x.Username, StringComparison.OrdinalIgnoreCase)).ConfigureAwait(false);
+                    AddResult(results, channelUser as T, channelUser?.Username == username ? 0.80f : 0.70f);
 
                     var guildUser = guildUsers.FirstOrDefault(x => x.DiscriminatorValue == discriminator &&
                         string.Equals(username, x.Username, StringComparison.OrdinalIgnoreCase));
@@ -64,10 +70,10 @@ namespace Fergun.Readers
 
             //By Username (0.5-0.6)
             {
-                //await channelUsers
-                //    .Where(x => string.Equals(input, x.Username, StringComparison.OrdinalIgnoreCase))
-                //    .ForEachAsync(channelUser => AddResult(results, channelUser as T, channelUser.Username == input ? 0.65f : 0.55f))
-                //    .ConfigureAwait(false);
+                await channelUsers
+                    .Where(x => string.Equals(input, x.Username, StringComparison.OrdinalIgnoreCase))
+                    .ForEachAsync(channelUser => AddResult(results, channelUser as T, channelUser.Username == input ? 0.60f : 0.50f))
+                    .ConfigureAwait(false);
 
                 foreach (var guildUser in guildUsers.Where(x => string.Equals(input, x.Username, StringComparison.OrdinalIgnoreCase)))
                     AddResult(results, guildUser as T, guildUser.Username == input ? 0.60f : 0.50f);
@@ -75,10 +81,10 @@ namespace Fergun.Readers
 
             //By Nickname (0.5-0.6)
             {
-                //await channelUsers
-                //    .Where(x => string.Equals(input, (x as IGuildUser)?.Nickname, StringComparison.OrdinalIgnoreCase))
-                //    .ForEachAsync(channelUser => AddResult(results, channelUser as T, (channelUser as IGuildUser).Nickname == input ? 0.65f : 0.55f))
-                //    .ConfigureAwait(false);
+                await channelUsers
+                    .Where(x => string.Equals(input, (x as IGuildUser)?.Nickname, StringComparison.OrdinalIgnoreCase))
+                    .ForEachAsync(channelUser => AddResult(results, channelUser as T, (channelUser as IGuildUser).Nickname == input ? 0.60f : 0.50f))
+                    .ConfigureAwait(false);
 
                 foreach (var guildUser in guildUsers.Where(x => string.Equals(input, x.Nickname, StringComparison.OrdinalIgnoreCase)))
                     AddResult(results, guildUser as T, guildUser.Nickname == input ? 0.60f : 0.50f);
