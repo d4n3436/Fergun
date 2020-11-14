f7423ad2d975bb61a1916a97c6af30ac447381ff
d4n <dan3436@hotmail.com>
2020-11-14 18:17:52 -0500

    Add Spotify command (spotify).

--- src/Modules/Music.cs
+++ src/Modules/Music.cs
@@ -20,15 +20,17 @@ namespace Fergun.Modules
 {
     [RequireBotPermission(Constants.MinimunRequiredPermissions)]
     [Ratelimit(Constants.GlobalCommandUsesPerPeriod, Constants.GlobalRatelimitPeriod, Measure.Minutes)]
-    [UserMustBeInVoice("lyrics")]
+    [UserMustBeInVoice("lyrics", "spotify")]
     public class Music : FergunBase
     {
         private readonly MusicService _musicService;
+        private static LogService _logService;
         private static GeniusApi _geniusApi;
 
-        public Music(MusicService musicService)
+        public Music(MusicService musicService, LogService logService)
         {
             _musicService = musicService;
+            _logService ??= logService;
             _geniusApi ??= new GeniusApi(FergunClient.Config.GeniusApiToken);
         }
 
@@ -238,6 +240,63 @@ namespace Fergun.Modules
                 var result2 = await _musicService.PlayTrack(Context.Guild, user.VoiceChannel, Context.Channel as ITextChannel, selectedTrack);
                 await SendEmbedAsync(result2);
             }
+            return FergunResult.FromSuccess();
+        }
+
+        [LongRunning]
+        [Command("spotify", RunMode = RunMode.Async)]
+        [Summary("spotifySummary")]
+        [Example("Fergun#6839")]
+        public async Task<RuntimeResult> Spotify([Remainder, Summary("spotifyParam1")] IUser user = null)
+        {
+            if (!FergunClient.Config.PresenceIntent)
+            {
+                return FergunResult.FromError(Locate("NoPresenceIntent"));
+            }
+
+            user ??= Context.User;
+            var spotify = user.Activities?.OfType<SpotifyGame>()?.FirstOrDefault();
+            if (spotify == null)
+            {
+                return FergunResult.FromError(string.Format(Locate("NoSpotifyStatus"), user));
+            }
+
+            string lyricsUrl = "?";
+            if (!string.IsNullOrEmpty(FergunClient.Config.GeniusApiToken))
+            {
+                GeniusResponse genius;
+                try
+                {
+                    genius = await _geniusApi.SearchAsync($"{string.Join(", ", spotify.Artists)} - {spotify.TrackTitle}");
+                    if (genius.Meta.Status == 200 && genius.Response.Hits.Count > 0)
+                    {
+                        lyricsUrl = Format.Url(Locate("ClickHere"), genius.Response.Hits[0].Result.Url);
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    await _logService.LogAsync(new LogMessage(LogSeverity.Warning, "Command", "Spotify: Error calling Genius API", e));
+                }
+            }
+
+            var builder = new EmbedBuilder()
+                .WithAuthor("Spotify", "https://i.ibb.co/XJQnL4j/spotify.png")
+                .WithThumbnailUrl(spotify.AlbumArtUrl)
+
+                .AddField(Locate("Title"), spotify.TrackTitle, true)
+                .AddField(Locate("Artist"), string.Join(", ", spotify.Artists), true)
+                .AddField("\u200b", "\u200b", true)
+
+                .AddField(Locate("Album"), spotify.AlbumTitle, true)
+                .AddField(Locate("Duration"), spotify.Duration?.ToShortForm() ?? "?", true)
+                .AddField("\u200b", "\u200b", true)
+
+                .AddField(Locate("Lyrics"), lyricsUrl, true)
+                .AddField(Locate("TrackUrl"), Format.Url(Locate("ClickHere"), spotify.TrackUrl), true)
+                .WithColor(FergunClient.Config.EmbedColor);
+
+            await ReplyAsync(embed: builder.Build());
+
             return FergunResult.FromSuccess();
         }
 
