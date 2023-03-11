4601aa6cb6c4c672f3f04ad297d9e5de709df045
d4n3436 <dan3436@hotmail.com>
2023-03-11 00:58:47 -0500

    Update GTranslate and refactor FergunTranslator

--- src/Entities/FergunTranslator.cs
+++ src/Entities/FergunTranslator.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Fergun.Extensions;
+﻿using Fergun.Extensions;
 using GTranslate;
 using GTranslate.Results;
 using GTranslate.Translators;
@@ -14,8 +13,8 @@ public class FergunTranslator : IFergunTranslator
     /// <inheritdoc/>
     public string Name => nameof(FergunTranslator);
 
-    private readonly ITranslator[] _translators;
-    private WrapBackEnumerable<ITranslator> _wrappedTranslators;
+    private readonly WrapBackCollection<ITranslator> _translators;
+    private readonly AggregateTranslator _innerTranslator;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="FergunTranslator"/> class.
@@ -27,246 +26,52 @@ public class FergunTranslator : IFergunTranslator
     public FergunTranslator(GoogleTranslator googleTranslator, GoogleTranslator2 googleTranslator2,
         MicrosoftTranslator microsoftTranslator, YandexTranslator yandexTranslator)
     {
-        _translators = new ITranslator[] {googleTranslator, googleTranslator2, microsoftTranslator, yandexTranslator};
-        _wrappedTranslators = new WrapBackEnumerable<ITranslator>(_translators);
+        _translators = new WrapBackCollection<ITranslator>(new ITranslator[]
+        {
+            googleTranslator,
+            googleTranslator2,
+            microsoftTranslator,
+            yandexTranslator
+        });
+
+        _innerTranslator = new AggregateTranslator(_translators);
     }
 
     /// <inheritdoc/>
     public void Next()
     {
-        _wrappedTranslators.Index = _wrappedTranslators.Index == _translators.Length - 1 ? 0 : _wrappedTranslators.Index + 1;
+        _translators.Index = _translators.Index == _translators.Count - 1 ? 0 : _translators.Index + 1;
     }
 
     /// <inheritdoc/>
     public void Randomize()
     {
-        _translators.Shuffle();
-        _wrappedTranslators.Index = Random.Shared.Next(0, _translators.Length);
+        _translators.Items.Shuffle();
+        _translators.Index = Random.Shared.Next(0, _translators.Count);
     }
 
-    /// <summary>
-    /// Translates a text using the available translation services.
-    /// </summary>
-    /// <param name="text">The text.</param>
-    /// <param name="toLanguage">The target language.</param>
-    /// <param name="fromLanguage">The source language.</param>
-    /// <returns>A task containing the translation result.</returns>
-    /// <remarks>This method will attempt to use all the translation services passed in the constructor, in the order they were provided.</remarks>
-    /// <exception cref="ObjectDisposedException">Thrown when this translator has been disposed.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> or <paramref name="toLanguage"/> are null.</exception>
-    /// <exception cref="TranslatorException">Thrown when no translator supports <paramref name="toLanguage"/> or <paramref name="fromLanguage"/>.</exception>
-    /// <exception cref="AggregateException">Thrown when all translators fail to provide a valid result.</exception>
-    public async Task<ITranslationResult> TranslateAsync(string text, string toLanguage, string? fromLanguage = null)
-    {
-        LanguageSupported(this, toLanguage, fromLanguage);
+    /// <inheritdoc />
+    public Task<ITranslationResult> TranslateAsync(string text, string toLanguage, string? fromLanguage = null)
+        => _innerTranslator.TranslateAsync(text, toLanguage, fromLanguage);
 
-        List<Exception> exceptions = null!;
-        foreach (var translator in _wrappedTranslators)
-        {
-            if (!translator.IsLanguageSupported(toLanguage) || fromLanguage != null && !translator.IsLanguageSupported(fromLanguage))
-            {
-                continue;
-            }
+    /// <inheritdoc />
+    public Task<ITranslationResult> TranslateAsync(string text, ILanguage toLanguage, ILanguage? fromLanguage = null)
+        => _innerTranslator.TranslateAsync(text, toLanguage, fromLanguage);
 
-            try
-            {
-                return await translator.TranslateAsync(text, toLanguage, fromLanguage).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                exceptions ??= new List<Exception>();
-                exceptions.Add(e);
-            }
-        }
+    /// <inheritdoc />
+    public Task<ITransliterationResult> TransliterateAsync(string text, string toLanguage, string? fromLanguage = null)
+        => _innerTranslator.TransliterateAsync(text, toLanguage, fromLanguage);
 
-        if (exceptions is null)
-        {
-            throw new TranslatorException($"No available translator supports the translation of the provided text from \"{fromLanguage}\" to \"{toLanguage}\".");
-        }
+    /// <inheritdoc />
+    public Task<ITransliterationResult> TransliterateAsync(string text, ILanguage toLanguage, ILanguage? fromLanguage = null)
+        => _innerTranslator.TransliterateAsync(text, toLanguage, fromLanguage);
 
-        throw new AggregateException("No translator provided a valid result.", exceptions);
-    }
+    /// <inheritdoc />
+    public Task<ILanguage> DetectLanguageAsync(string text) => _innerTranslator.DetectLanguageAsync(text);
 
-    /// <inheritdoc cref="TranslateAsync(string, string, string)"/>
-    public async Task<ITranslationResult> TranslateAsync(string text, ILanguage toLanguage, ILanguage? fromLanguage = null)
-    {
-        LanguageSupported(this, toLanguage, fromLanguage);
+    /// <inheritdoc />
+    public bool IsLanguageSupported(string language) => _innerTranslator.IsLanguageSupported(language);
 
-        List<Exception> exceptions = null!;
-        foreach (var translator in _wrappedTranslators)
-        {
-            if (!translator.IsLanguageSupported(toLanguage) || fromLanguage != null && !translator.IsLanguageSupported(fromLanguage))
-            {
-                continue;
-            }
-
-            try
-            {
-                return await translator.TranslateAsync(text, toLanguage, fromLanguage).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                exceptions ??= new List<Exception>();
-                exceptions.Add(e);
-            }
-        }
-
-        if (exceptions is null)
-        {
-            throw new TranslatorException($"No available translator supports the translation of the provided text from \"{fromLanguage}\" to \"{toLanguage}\".");
-        }
-
-        throw new AggregateException("No translator provided a valid result.", exceptions);
-    }
-
-    /// <summary>
-    /// Transliterates a text using the available translation services.
-    /// </summary>
-    /// <param name="text">The text.</param>
-    /// <param name="toLanguage">The target language.</param>
-    /// <param name="fromLanguage">The source language.</param>
-    /// <returns>A task containing the transliteration result.</returns>
-    /// <remarks>This method will attempt to use all the translation services passed in the constructor, in the order they were provided.</remarks>
-    /// <exception cref="ObjectDisposedException">Thrown when this translator has been disposed.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> or <paramref name="toLanguage"/> are null.</exception>
-    /// <exception cref="ArgumentException">Thrown when a <see cref="Language"/> could not be obtained from <paramref name="toLanguage"/> or <paramref name="fromLanguage"/>.</exception>
-    /// <exception cref="TranslatorException">Thrown when no translator supports <paramref name="toLanguage"/> or <paramref name="fromLanguage"/>.</exception>
-    /// <exception cref="AggregateException">Thrown when all translators fail to provide a valid result.</exception>
-    public async Task<ITransliterationResult> TransliterateAsync(string text, string toLanguage, string? fromLanguage = null)
-    {
-        LanguageFound(toLanguage, out var toLang, "Unknown target language.");
-        LanguageFound(fromLanguage, out var fromLang, "Unknown source language.");
-
-        return await TransliterateAsync(text, toLang, fromLang).ConfigureAwait(false);
-    }
-
-    /// <inheritdoc cref="TransliterateAsync(string, string, string)"/>
-    public async Task<ITransliterationResult> TransliterateAsync(string text, ILanguage toLanguage, ILanguage? fromLanguage = null)
-    {
-        LanguageSupported(this, toLanguage, fromLanguage);
-
-        List<Exception> exceptions = null!;
-        foreach (var translator in _wrappedTranslators)
-        {
-            if (!translator.IsLanguageSupported(toLanguage) || fromLanguage != null && !translator.IsLanguageSupported(fromLanguage))
-            {
-                continue;
-            }
-
-            try
-            {
-                return await translator.TransliterateAsync(text, toLanguage, fromLanguage).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                exceptions ??= new List<Exception>();
-                exceptions.Add(e);
-            }
-        }
-
-        if (exceptions is null)
-        {
-            throw new TranslatorException($"No available translator supports the transliteration of the provided text from \"{fromLanguage}\" to \"{toLanguage}\".");
-        }
-
-        throw new AggregateException("No translator provided a valid result.", exceptions);
-    }
-
-    /// <summary>
-    /// Detects the language of a text using the available translation services.
-    /// </summary>
-    /// <param name="text">The text to detect its language.</param>
-    /// <returns>A task that represents the asynchronous language detection operation. The task contains the detected language.</returns>
-    /// <remarks>This method will attempt to use all the translation services passed in the constructor, in the order they were provided.</remarks>
-    /// <exception cref="ObjectDisposedException">Thrown when this translator has been disposed.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> is null.</exception>
-    /// <exception cref="AggregateException">Thrown when all translators fail to provide a valid result.</exception>
-    public async Task<ILanguage> DetectLanguageAsync(string text)
-    {
-        List<Exception> exceptions = null!;
-        foreach (var translator in _wrappedTranslators)
-        {
-            try
-            {
-                return await translator.DetectLanguageAsync(text).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                exceptions ??= new List<Exception>();
-                exceptions.Add(e);
-            }
-        }
-
-        throw new AggregateException("No translator provided a valid result.", exceptions);
-    }
-
-    /// <summary>
-    /// Returns whether at least one translator supports the specified language.
-    /// </summary>
-    /// <param name="language">The language.</param>
-    /// <returns><see langword="true"/> if the language is supported by at least one translator, otherwise <see langword="false"/>.</returns>
-    public bool IsLanguageSupported(string language)
-    {
-        foreach (var translator in _wrappedTranslators)
-        {
-            if (translator.IsLanguageSupported(language))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /// <inheritdoc cref="IsLanguageSupported(string)"/>
-    public bool IsLanguageSupported(ILanguage language)
-    {
-        foreach (var translator in _wrappedTranslators)
-        {
-            if (translator.IsLanguageSupported(language))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static void LanguageFound(string? language, out Language lang, string message = "Unknown language.",
-        [CallerArgumentExpression("language")] string? parameterName = null)
-    {
-        Language temp = null!;
-        if (language is not null && !Language.TryGetLanguage(language, out temp!))
-        {
-            throw new ArgumentException(message, parameterName);
-        }
-
-        lang = temp;
-    }
-
-    private static void LanguageSupported(ITranslator translator, string toLanguage, string? fromLanguage)
-    {
-        if (!translator.IsLanguageSupported(toLanguage))
-        {
-            throw new TranslatorException($"No available translator supports the target language \"{toLanguage}\".", translator.Name);
-        }
-
-        if (!string.IsNullOrEmpty(fromLanguage) && !translator.IsLanguageSupported(fromLanguage))
-        {
-            throw new TranslatorException($"No available translator supports the source language \"{fromLanguage}\".", translator.Name);
-        }
-    }
-
-    private static void LanguageSupported(ITranslator translator, ILanguage toLanguage, ILanguage? fromLanguage)
-    {
-        if (!translator.IsLanguageSupported(toLanguage))
-        {
-            throw new TranslatorException($"No available translator supports the target language \"{toLanguage.ISO6391}\".", translator.Name);
-        }
-
-        if (fromLanguage != null && !translator.IsLanguageSupported(fromLanguage))
-        {
-            throw new TranslatorException($"No available translator supports the source language \"{fromLanguage.ISO6391}\".", translator.Name);
-        }
-    }
+    /// <inheritdoc />
+    public bool IsLanguageSupported(ILanguage language) => _innerTranslator.IsLanguageSupported(language);
 }
--- src/Entities/WrapBackCollection.cs
+++ src/Entities/WrapBackCollection.cs
@@ -1,18 +1,20 @@
-﻿namespace Fergun;
+﻿using System.Collections;
+
+namespace Fergun;
 
 /// <summary>
 /// Provides an enumerator that wraps back to the start after reaching the last element.
 /// </summary>
 /// <typeparam name="T">The type of the elements to enumerate.</typeparam>
-public struct WrapBackEnumerable<T>
+public class WrapBackCollection<T> : IReadOnlyCollection<T>
 {
     private readonly T[] _items;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="WrapBackEnumerable{T}"/> struct.
+    /// Initializes a new instance of the <see cref="WrapBackCollection{T}"/> struct.
     /// </summary>
     /// <param name="items">The items.</param>
-    public WrapBackEnumerable(T[] items)
+    public WrapBackCollection(T[] items)
     {
         _items = items;
         Index = 0;
@@ -23,32 +25,38 @@ public struct WrapBackEnumerable<T>
     /// </summary>
     public int Index { get; set; }
 
-    /// <summary>
-    /// Returns the enumerator.
-    /// </summary>
-    /// <returns>The enumerator.</returns>
-    public readonly Enumerator GetEnumerator() => new(_items,  Index);
+    /// <inheritdoc/>
+    public int Count => _items.Length;
 
     /// <summary>
-    /// Enumerates the elements of a <see cref="WrapBackEnumerable{T}"/>.
+    /// Gets the items.
     /// </summary>
-    public struct Enumerator
+    public IList<T> Items => _items;
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator() => new WrapBackEnumerator(_items, Index);
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    /// <summary>
+    /// Enumerates the elements of a <see cref="WrapBackCollection{T}"/>.
+    /// </summary>
+    private sealed class WrapBackEnumerator : IEnumerator<T>
     {
         private readonly IReadOnlyList<T> _items;
         private int _index;
         private int _increment;
 
         
-        internal Enumerator(IReadOnlyList<T> items, int index)
+        internal WrapBackEnumerator(IReadOnlyList<T> items, int index)
         {
             _items = items;
             _index = index;
             _increment = 0;
         }
 
-        /// <summary>
-        /// Advances the enumerator to the next element.
-        /// </summary>
+        /// <inheritdoc/>
         public bool MoveNext()
         {
             if (_increment >= _items.Count)
@@ -62,9 +70,20 @@ public struct WrapBackEnumerable<T>
             return true;
         }
 
+        /// <inheritdoc/>
+        public void Reset() => throw new NotSupportedException();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+        }
+
         /// <summary>
         /// Gets the current element.
         /// </summary>
-        public readonly T Current => _items[_index];
+        public T Current => _items[_index];
+
+        /// <inheritdoc/>
+        object IEnumerator.Current => Current!;
     }
 }
