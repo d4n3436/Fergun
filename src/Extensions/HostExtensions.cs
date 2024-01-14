0cb16c884ee985ed1d87fcb81a68cd1e5733eccd
d4n3436 <dan3436@hotmail.com>
2024-01-14 16:29:30 -0500

    Use `IHostApplicationBuilder` configuration model

---
+++ src/Extensions/HostExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Fergun.Data;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Fergun.Extensions;
+
+/// <summary>
+/// Contains extension methods for <see cref="IHost"/>.
+/// </summary>
+public static class HostExtensions
+{
+    /// <summary>
+    /// Applies all pending migrations. The database will be created if it does not already exist.
+    /// </summary>
+    /// <param name="host">The host.</param>
+    /// <returns>The host.</returns>
+    public static IHost ApplyMigrations(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<FergunContext>();
+        int pendingMigrations = db.Database.GetPendingMigrations().Count();
+
+        if (pendingMigrations > 0)
+        {
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            db.Database.Migrate();
+            logger.LogInformation("Applied {Migrations}.", "pending database migration".ToQuantity(pendingMigrations));
+        }
+
+        return host;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using Discord;
 using Discord.Addons.Hosting;
-using Discord.Interactions;
 using Discord.WebSocket;
 using Fergun;
 using Fergun.Apis.Bing;
@@ -27,7 +25,6 @@ using Fergun.Services;
 using GScraper.DuckDuckGo;
 using GScraper.Google;
 using GTranslate.Translators;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -42,181 +39,95 @@ using YoutubeExplode.Search;
 // instead of the project folder (if the data source path is relative).
 Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 
-var host = Host.CreateDefaultBuilder()
-    .UseConsoleLifetime()
-    .UseContentRoot(AppDomain.CurrentDomain.BaseDirectory)
-    .ConfigureServices((context, services) =>
+var builder = Host.CreateApplicationBuilder();
+
+TypeDescriptor.AddAttributes(typeof(IEmote), new TypeConverterAttribute(typeof(EmoteConverter)));
+builder.Services.AddOptions<StartupOptions>().BindConfiguration(StartupOptions.Startup)
+    .PostConfigure(startup =>
     {
-        TypeDescriptor.AddAttributes(typeof(IEmote), new TypeConverterAttribute(typeof(EmoteConverter)));
-        services.AddOptions<StartupOptions>()
-            .BindConfiguration(StartupOptions.Startup)
-            .PostConfigure(startup =>
-            {
-                if (startup.MobileStatus)
-                {
-                    MobilePatcher.Patch();
-                }
-            });
-
-        services.AddOptions<BotListOptions>()
-            .BindConfiguration(BotListOptions.BotList);
-
-        services.AddOptions<FergunOptions>()
-            .BindConfiguration(FergunOptions.Fergun);
-
-        services.AddSqlite<FergunContext>(context.Configuration.GetConnectionString("FergunDatabase"));
-    })
-    .ConfigureDiscordShardedHost((context, config) =>
-    {
-        config.SocketConfig = new DiscordSocketConfig
+        if (startup.MobileStatus)
         {
-            LogLevel = LogSeverity.Verbose,
-            GatewayIntents = GatewayIntents.Guilds,
-            UseInteractionSnowflakeDate = false,
-            LogGatewayIntentWarnings = false,
-            SuppressUnknownDispatchWarnings = true,
-            FormatUsersInBidirectionalUnicode = false
-        };
+            MobilePatcher.Patch();
+        }
+    });
+builder.Services.AddOptions<BotListOptions>().BindConfiguration(BotListOptions.BotList);
+builder.Services.AddOptions<FergunOptions>().BindConfiguration(FergunOptions.Fergun);
 
-        config.Token = context.Configuration.GetSection(StartupOptions.Startup).Get<StartupOptions>()!.Token;
-    })
-    .UseInteractionService((_, config) =>
-    {
-        config.LogLevel = LogSeverity.Critical;
-        config.DefaultRunMode = RunMode.Async;
-        config.UseCompiledLambda = false;
-    })
-    .ConfigureLogging(logging => logging.ClearProviders())
-    .UseSerilog((context, config) =>
-    {
-        config.MinimumLevel.Debug()
-            .Filter.ByExcluding(e => e.Level == LogEventLevel.Debug && Matching.FromSource("Discord.WebSocket.DiscordShardedClient").Invoke(e) && e.MessageTemplate.Render(e.Properties).ContainsAny("Connected to", "Disconnected from"))
-            .Filter.ByExcluding(e => e.Level <= LogEventLevel.Debug && (Matching.FromSource("Microsoft.Extensions.Http").Invoke(e) || Matching.FromSource("Microsoft.Extensions.Localization").Invoke(e)))
-            .Filter.ByExcluding(e => e.Level <= LogEventLevel.Information && Matching.FromSource("Microsoft.EntityFrameworkCore").Invoke(e))
-            .WriteTo.Console(LogEventLevel.Debug, theme: AnsiConsoleTheme.Literate)
-            .WriteTo.Async(logger => logger.File($"{context.HostingEnvironment.ContentRootPath}logs/log-.txt", LogEventLevel.Debug, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null));
-    })
-    .ConfigureServices(services =>
-    {
-        services.AddLocalization(options => options.ResourcesPath = "Resources");
-        services.AddTransient(typeof(IFergunLocalizer<>), typeof(FergunLocalizer<>));
-        services.AddSingleton<FergunLocalizationManager>();
-        services.AddHostedService<InteractionHandlingService>();
-        services.AddHostedService<BotListService>();
-        services.ConfigureHttpClientDefaults(b =>
-        {
-            b.RemoveAllLoggers();
-            b.AddLogger<FergunHttpClientLogger>();
-        });
-        services.AddSingleton(new InteractiveConfig { ReturnAfterSendingPaginator = true, DeferStopSelectionInteractions = false });
-        services.AddSingleton<InteractiveService>();
-        services.AddSingleton<FergunHttpClientLogger>();
-        services.AddHostedService<InteractiveServiceLoggerHost>();
-        services.AddSingleton<MusixmatchClientState>();
-        services.AddFergunPolicies();
+builder.Services.AddSqlite<FergunContext>(builder.Configuration.GetConnectionString("FergunDatabase"));
 
-        services.AddHttpClient<IBingVisualSearch, BingVisualSearch>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IYandexImageSearch, YandexImageSearch>()
-            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false })
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IGoogleLensClient, GoogleLensClient>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IUrbanDictionary, UrbanDictionary>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IWikipediaClient, WikipediaClient>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IDictionaryClient, DictionaryClient>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IWolframAlphaClient, WolframAlphaClient>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IGeniusClient, GeniusClient>()
-            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false })
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<IMusixmatchClient, MusixmatchClient>()
-            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false })
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<ITranslator, GoogleTranslator>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<ITranslator, GoogleTranslator2>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        // Registered twice so the one added as "itself" can be used in SharedModule
-        services.AddHttpClient<GoogleTranslator2>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<ITranslator, YandexTranslator>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<ITranslator, MicrosoftTranslator>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        // Singleton used in TtsModule and MicrosoftVoiceConverter
-        services.AddSingleton(s => new MicrosoftTranslator(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(MicrosoftTranslator))));
-
-        services.AddHttpClient<SearchClient>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient<OtherModule>()
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient(nameof(GoogleScraper))
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient(nameof(DuckDuckGoScraper))
-            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false })
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30))
-            .AddRetryPolicy();
-
-        services.AddHttpClient("autocomplete", client => client.DefaultRequestHeaders.UserAgent.ParseAdd(Constants.ChromeUserAgent))
-            .SetHandlerLifetime(TimeSpan.FromMinutes(30));
-
-        services.AddTransient<IFergunTranslator, FergunTranslator>();
-        services.AddTransient(s => new GoogleScraper(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(GoogleScraper))));
-        services.AddTransient(s => new DuckDuckGoScraper(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(DuckDuckGoScraper))));
-        services.AddTransient<SharedModule>();
-    })
-    .Build();
-
-// Semi-automatic migration
-await using (var scope = host.Services.CreateAsyncScope())
+builder.Services.AddDiscordShardedHost((config, _) =>
 {
-    var db = scope.ServiceProvider.GetRequiredService<FergunContext>();
-    int pendingMigrations = (await db.Database.GetPendingMigrationsAsync()).Count();
-
-    if (pendingMigrations > 0)
+    config.SocketConfig = new DiscordSocketConfig
     {
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-        await db.Database.MigrateAsync();
-        logger.LogInformation("Applied {Count} pending database migration(s).", pendingMigrations);
-    }
-}
+        LogLevel = LogSeverity.Verbose,
+        GatewayIntents = GatewayIntents.Guilds,
+        UseInteractionSnowflakeDate = false,
+        LogGatewayIntentWarnings = false,
+        FormatUsersInBidirectionalUnicode = false
+    };
+
+    config.Token = builder.Configuration.GetSection(StartupOptions.Startup).Get<StartupOptions>()!.Token;
+});
+
+builder.Services.AddInteractionService((config, _) => config.LogLevel = LogSeverity.Critical);
+
+builder.Logging.ClearProviders();
+builder.Services.AddSerilog(config =>
+{
+    config.MinimumLevel.Debug()
+        .Filter.ByExcluding(e => e.Level == LogEventLevel.Debug && Matching.FromSource("Discord.WebSocket.DiscordShardedClient").Invoke(e) && e.MessageTemplate.Render(e.Properties).ContainsAny("Connected to", "Disconnected from"))
+        .Filter.ByExcluding(e => e.Level <= LogEventLevel.Debug && (Matching.FromSource("Microsoft.Extensions.Http").Invoke(e) || Matching.FromSource("Microsoft.Extensions.Localization").Invoke(e)))
+        .Filter.ByExcluding(e => e.Level <= LogEventLevel.Information && Matching.FromSource("Microsoft.EntityFrameworkCore").Invoke(e))
+        .WriteTo.Console(LogEventLevel.Debug, theme: AnsiConsoleTheme.Literate)
+        .WriteTo.Async(logger => logger.File(Path.Combine(builder.Environment.ContentRootPath, "logs", "log-.txt"), LogEventLevel.Debug, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null));
+});
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddTransient(typeof(IFergunLocalizer<>), typeof(FergunLocalizer<>));
+builder.Services.AddSingleton<FergunLocalizationManager>();
+builder.Services.AddHostedService<InteractionHandlingService>();
+builder.Services.AddHostedService<BotListService>();
+builder.Services.ConfigureHttpClientDefaults(b =>
+{
+    b.AddRetryPolicy();
+    b.RemoveAllLoggers();
+    b.AddLogger<FergunHttpClientLogger>();
+});
+builder.Services.AddSingleton(new InteractiveConfig { ReturnAfterSendingPaginator = true, DeferStopSelectionInteractions = false });
+builder.Services.AddSingleton<InteractiveService>();
+builder.Services.AddSingleton<FergunHttpClientLogger>();
+builder.Services.AddHostedService<InteractiveServiceLoggerHost>();
+builder.Services.AddSingleton<MusixmatchClientState>();
+builder.Services.AddFergunPolicies();
+
+builder.Services.AddHttpClient<IBingVisualSearch, BingVisualSearch>();
+builder.Services.AddHttpClient<IYandexImageSearch, YandexImageSearch>().ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false });
+builder.Services.AddHttpClient<IGoogleLensClient, GoogleLensClient>();
+builder.Services.AddHttpClient<IUrbanDictionary, UrbanDictionary>();
+builder.Services.AddHttpClient<IWikipediaClient, WikipediaClient>();
+builder.Services.AddHttpClient<IDictionaryClient, DictionaryClient>();
+builder.Services.AddHttpClient<IWolframAlphaClient, WolframAlphaClient>();
+builder.Services.AddHttpClient<IGeniusClient, GeniusClient>().ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false });
+builder.Services.AddHttpClient<IMusixmatchClient, MusixmatchClient>().ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false });
+builder.Services.AddHttpClient<ITranslator, GoogleTranslator>();
+builder.Services.AddHttpClient<ITranslator, GoogleTranslator2>();
+builder.Services.AddHttpClient<GoogleTranslator2>(); // Registered twice so the one added as "itself" can be used in SharedModule
+builder.Services.AddHttpClient<ITranslator, YandexTranslator>();
+builder.Services.AddHttpClient<ITranslator, MicrosoftTranslator>();
+builder.Services.AddHttpClient<SearchClient>();
+builder.Services.AddHttpClient<OtherModule>();
+builder.Services.AddHttpClient(nameof(GoogleScraper));
+builder.Services.AddHttpClient(nameof(DuckDuckGoScraper)).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false });
+builder.Services.AddHttpClient("autocomplete", client => client.DefaultRequestHeaders.UserAgent.ParseAdd(Constants.ChromeUserAgent));
+
+builder.Services.AddSingleton(s => new MicrosoftTranslator(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(MicrosoftTranslator)))); // Singleton used in TtsModule and MicrosoftVoiceConverter
+builder.Services.AddTransient<IFergunTranslator, FergunTranslator>();
+builder.Services.AddTransient(s => new GoogleScraper(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(GoogleScraper))));
+builder.Services.AddTransient(s => new DuckDuckGoScraper(s.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(DuckDuckGoScraper))));
+builder.Services.AddTransient<SharedModule>();
+
+var host = builder.Build();
+
+host.ApplyMigrations();
 
 await host.RunAsync();
